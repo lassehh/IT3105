93682f0c1bbe336245a9aafd045e9b47b9acecda
Lasse <lassehhenriksen@gmail.com>
2017-09-04 17:29:06 +0200

    Module 1 completed (with colors)
    Remaining work:
    - Create display mode
    - Expand the calc_h function(?)

--- Project 1/src/GenericAStar.py
+++ Project 1/src/GenericAStar.py
@@ -4,50 +4,58 @@ class AStar:
 	closed_list = []  				# Visited nodes
 	n0 = None
 	start = None
-	search_type = None
+	searchType = None
+
+	searchNodesGenerated = 0
 
 	def __init__(self, search_type, start_search_node):
 		self.start = start_search_node
-		self.search_type = search_type
+		self.searchType = search_type
 		self.created_dict = {(self.start.state): self.start}
 		self.open_list = []
 		self.closed_list = []
+		self.searchNodesGenerated = 0
 
 	def best_first_search(self):
-		n0 = self.start
-		n0.g = 0
-		n0.calc_h()
-		n0.f = n0.h + n0.g
+		self.n0 = self.start
+		self.n0.g = 0
+		self.n0.calc_h()
+		self.n0.f = self.n0.h + self.n0.g
 
-		self.open_list.append(n0)
+		self.open_list.append(self.n0)
+		self.searchNodesGenerated = 1
 
 		# Agenda Loop
 		while (self.open_list):
-			x = self.search_queue_pop(self.search_type, self.open_list)
+			x = self.search_queue_pop(self.searchType, self.open_list)
+			#x.display_game_board()
 			self.closed_list.append(x)
 			if x.is_goal():
 				return x
 			successors = x.generate_successors()
 
 			for s in successors:
+				#s.display_game_board()
+				x.kids.append(s)
 				if (s.state) in self.created_dict:
 					s = self.created_dict[(s.state)]
 				if s not in self.open_list and s not in self.closed_list:
 					self.attach_and_eval(s, x)
 					self.open_list.append(s)
 					self.created_dict[(s.state)] = s
+					self.searchNodesGenerated += 1
 				elif x.g + x.arc_cost() < s.g:
 					self.attach_and_eval(s, x)
 					if s in self.closed_list:
 						self.propagate_path_improvements(s)
 		return []
 
-	def search_queue_pop(self, search_type, queue):  #
-		if search_type == "BFS":  # Breadth First Search
+	def search_queue_pop(self, searchType, queue):  #
+		if searchType == "BFS":  # Breadth First Search
 			return queue.pop(0)
-		elif search_type == "DFS":  # Depth First Search
+		elif searchType == "DFS":  # Depth First Search
 			return queue.pop()
-		elif search_type == "BestFS":  # Best First Search
+		elif searchType == "BestFS":  # Best First Search
 			current_node = min(queue, key=lambda x: x.f)
 			queue.remove(current_node)
 			return current_node
@@ -57,7 +65,7 @@ class AStar:
 	def attach_and_eval(self, c, p):
 		c.parent = p
 		c.g = p.g + p.arc_cost()
-		c.h = c.calc_h()
+		c.calc_h()
 		c.f = c.g + c.h
 
 	def propagate_path_improvements(self, p):
@@ -67,6 +75,14 @@ class AStar:
 				c.g = p.g + p.arc_cost()
 				c.f = c.g + c.h
 				self.propagate_path_improvements(c)
+
+	def get_number_of_moves(self, solutionNode):
+		numberOfMoves = 0
+		while(solutionNode.parent != None):
+			numberOfMoves += 1
+			solutionNode = solutionNode.parent
+		return numberOfMoves
+
 
 	# def path(self, x):
 	# 	goal_path = [x.state]
--- Project 1/src/RushHourGame.py
+++ Project 1/src/RushHourGame.py
@@ -1,5 +1,7 @@
 import sys
 import numpy as np
+from termcolor import colored, cprint
+import colorama
 from string import *
 
 
@@ -9,6 +11,7 @@ class RushHourGameNode:
     gameBoardSize = []
     numberOfVehicles = 0
     vehicles = []
+    vehicleColors = None
 
     start = None
     goal = None
@@ -22,16 +25,18 @@ class RushHourGameNode:
 
 
     def __init__(self, boardLength = 6, boardHeight = 6, vehicles = [], numberOfVehicles = 0, state = '', goal = (5,2)):
-        self.gameBoard = np.zeros((boardLength, boardHeight))
-        self.gameBoardSize = (boardLength,boardHeight)
-        self.numberOfVehicles = numberOfVehicles
         self.vehicles = vehicles
+        self.vehicleColors = {("0"): "red", ("1"): "yellow", ("2"): "magenta", ("3"): "green", ("4"): "cyan", ("5"): "blue",
+                              ("6"): "green", ("7"): "yellow", ("8"): "magenta", ("9"): "green", ("10"): "magenta",
+                              ("11"): "cyan", ("12"): "green", ("x"): "white"}
+        self.numberOfVehicles = numberOfVehicles
+        self.gameBoardSize = (boardLength,boardHeight)
+        self.update_game_board()
         self.goal = goal
         self.state = state
         self.parent = None
         self.kids = []
         self.g = 0
-
 
     def load_game_configuration(self, fileName):
         with open('../game_configurations/' + fileName + '.txt', 'r') as f:
@@ -45,21 +50,22 @@ class RushHourGameNode:
                 self.numberOfVehicles += 1
 
     def update_game_board(self):
-        self.gameBoard = np.zeros(shape = self.gameBoardSize, dtype = str)
+        self.gameBoard = np.zeros(shape = self.gameBoardSize, dtype = object)
         self.gameBoard[:] = 'x'
         for vehicle in self.vehicles:
             (number, orientation, xPos, yPos, size)  = vehicle[0], vehicle[1], vehicle[2], vehicle[3], vehicle[4]
             if(orientation == 0):
-                self.gameBoard[yPos, xPos:xPos+size] = number
+                self.gameBoard[yPos, xPos:xPos+size] = str(number)
             elif(orientation == 1):
-                self.gameBoard[yPos:yPos + size, xPos] = number
+                self.gameBoard[yPos:yPos + size, xPos] = str(number)
 
     def display_game_board(self):
-        # Display the board on screen
         for row in self.gameBoard:
             for element in row:
-                print(element + ' ', end='')
+                #if (element) in self.vehicleColors:
+                print(colored((element + ' '), self.vehicleColors[str(element)]), end ='')
             print('')
+        print('\n')
 
     def get_state_identifier(self, vehicles):
         state = ''
@@ -68,8 +74,23 @@ class RushHourGameNode:
         return state
 
     def calc_h(self):
-        self.h = 0
-        return 0
+        estimatedMovesToSolution = 0
+        objectiveVehicle = self.vehicles[0]
+        (xPos, yPos, size) = objectiveVehicle[2], objectiveVehicle[3], objectiveVehicle[4]
+        step = 1
+
+        while(xPos + (size - 1) + step < 6):
+            if(self.gameBoard[yPos, xPos + (size - 1) + step] != 'x'):
+                estimatedMovesToSolution += 1
+            step += 1
+        estimatedMovesToSolution += step
+
+        if(estimatedMovesToSolution == 0 or estimatedMovesToSolution < 0):
+            self.h = 1
+        else:
+            self.h = estimatedMovesToSolution
+
+
 
     def arc_cost(self):
         return 1
@@ -96,33 +117,29 @@ class RushHourGameNode:
                     newGameNode = RushHourGameNode(vehicles = newVehicleConfig, numberOfVehicles = self.numberOfVehicles,
                                                    state = self.get_state_identifier(newVehicleConfig))
                     succesors.append(newGameNode)
-                    continue
                 # The right square is free and is not outside the game board
-                elif(xPos + size - 1 < 5 and self.gameBoard[yPos,xPos + size] == 'x'):
+                if (xPos + size - 1 < 5 and self.gameBoard[yPos, xPos + size] == 'x'):
                     newVehicleConfig = list(self.vehicles)
                     newVehicleConfig[number] = [number, orientation, xPos + 1, yPos, size]
                     newGameNode = RushHourGameNode(vehicles=newVehicleConfig, numberOfVehicles=self.numberOfVehicles,
                                                    state=self.get_state_identifier(newVehicleConfig))
                     succesors.append(newGameNode)
-                    continue
             # Vertical vehicle
             elif(orientation == 1):
                 # The square above is free and is not outside the game board
-                if (yPos > 0 and self.gameBoard[yPos - 1, xPos] == 'x'):
+                if(yPos > 0 and self.gameBoard[yPos - 1, xPos] == 'x'):
                     newVehicleConfig = list(self.vehicles)
                     newVehicleConfig[number] = [number, orientation, xPos, yPos - 1, size]
-                    newGameNode = RushHourGameNode(vehicles=newVehicleConfig, numberOfVehicles=self.numberOfVehicles,
-                                                   state=self.get_state_identifier(newVehicleConfig))
+                    newGameNode = RushHourGameNode(vehicles = newVehicleConfig, numberOfVehicles = self.numberOfVehicles,
+                                                   state = self.get_state_identifier(newVehicleConfig))
                     succesors.append(newGameNode)
-                    continue
                 # The square below is free and is not outside the game board
-                elif (yPos + size - 1 < 5 and self.gameBoard[yPos + size, xPos] == 'x'):
+                if(yPos + size - 1 < 5 and self.gameBoard[yPos + size, xPos] == 'x'):
                     newVehicleConfig = list(self.vehicles)
                     newVehicleConfig[number] = [number, orientation, xPos, yPos + 1, size]
-                    newGameNode = RushHourGameNode(vehicles=newVehicleConfig, numberOfVehicles=self.numberOfVehicles,
-                                                   state=self.get_state_identifier(newVehicleConfig))
+                    newGameNode = RushHourGameNode(vehicles = newVehicleConfig, numberOfVehicles = self.numberOfVehicles,
+                                                   state = self.get_state_identifier(newVehicleConfig))
                     succesors.append(newGameNode)
-                    continue
         return succesors
 
 
