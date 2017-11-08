ed8c93105df3920af505f05cd3d75a7cbc09eff4
ingunnjv <ingunn.vallestad@gmail.com>
2017-11-08 23:52:16 +0100

    Add new TSP files and modify the data generating function

--- Project_3/src/miscfunctions.py
+++ Project_3/src/miscfunctions.py
@@ -2,19 +2,27 @@ import numpy as np
 
 
 def generate_tsp_data(problemNumbr):
-    with open('../data/TSP_Problems/' + str(problemNumbr) + '.txt', 'r') as f:
+    with open('../data/TSP_Problems_Euclidean/' + str(problemNumbr) + '.txt', 'r') as f:
         firstLine = f.readline().strip("\n")
         firstLine = firstLine.split(":")
-        nmbrOfCities = firstLine[1]
+        name = firstLine[1].strip()
+        secondLine = f.readline().strip("\n")
+        secondLine = secondLine.split(":")
+        problemType = secondLine[1].strip()
+        thirdLine = f.readline().strip("\n")
+        thirdLine = thirdLine.split(":")
+        nmbrOfCities = thirdLine[1].strip()
         cities = np.zeros((int(nmbrOfCities), 2))
+        # Skip two lines
+        f.__next__()
         f.__next__()
         for line in f:
             line = line.strip("\n")
             if(line == "EOF"):
                 break
-            line = line.split(" ")
+            line = line.split()
             cityNmbr, x, y = line
-            cityNmbr, x, y = int(cityNmbr), int(x), int(y)
+            cityNmbr, x, y = int(cityNmbr), float(x), float(y)
             vectorNorm = 1 #np.linalg.norm(np.array([x,y]))
             x_normed, y_normed = x/vectorNorm, y/vectorNorm
             cities[cityNmbr-1,:] = x_normed, y_normed
